6212e1c833408feedf87bb68cbdf635f86fab223
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-22 19:55:13 +0530

    Insertion sort

---
+++ DSA/insertionSort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i,n,j,temp;
+    printf("Enter the array size: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter number in array: ");
+    for(i=0;i<n;i++){
+        scanf("%d", &a[i]);
+    }
+    for(i=1;i<n;i++){
+        for(j=i;j>=1;j--){
+            if(a[j-1]>a[j]){
+                temp = a[j-1];
+                a[j-1]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("The sorted list is: ");
+    for(i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+    return 0;
+}
