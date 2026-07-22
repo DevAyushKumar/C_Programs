d577b50ab67406e3d2c79ff7da589c5a9511ec6a
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-22 18:30:13 +0530

    Bubble sort

---
+++ DSA/bubbleSort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int i,n,j,temp;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter values in array: ");
+    for(i=0; i<n; ++i){
+        scanf("%d",&a[i]);
+    }
+    for(i=n; i>0; i--){
+        for(j=0; j<i-1; j++){
+            if(a[j]>a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted result: \n");
+    for(i=0; i<n; ++i){
+        printf("%d", a[i]);
+    }
+    return 0;
+}
